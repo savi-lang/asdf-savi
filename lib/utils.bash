44d8bca67e41bf0c3385bcebc64585211ba59b9e
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2022-03-15 18:20:37 -0700

    Remove `v` version prefix from tag names.

--- lib/utils.bash
+++ lib/utils.bash
@@ -32,7 +32,8 @@ sort_versions() {
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3-
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' # remove version prefix
 }
 
 list_all_versions() {
